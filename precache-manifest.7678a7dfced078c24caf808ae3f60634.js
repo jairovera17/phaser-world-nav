bc3a1ce54aace294b9160a2db23b1bc131bf6823
jairovera17 <jairovera17@gmail.com>
2020-07-03 17:44:08 -0500

    Updates

---
+++ precache-manifest.7678a7dfced078c24caf808ae3f60634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073560ad355b0937873d3f4ec7f6c7bb",
+    "url": "/phaser-world-nav/index.html"
+  },
+  {
+    "revision": "fe2923358905b699d4f5",
+    "url": "/phaser-world-nav/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "313e06b03c250ee2fb53",
+    "url": "/phaser-world-nav/static/js/2.05209382.chunk.js"
+  },
+  {
+    "revision": "19436aa5872d75cdbbc02e6c3c56f9c3",
+    "url": "/phaser-world-nav/static/js/2.05209382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2923358905b699d4f5",
+    "url": "/phaser-world-nav/static/js/main.d6d54503.chunk.js"
+  },
+  {
+    "revision": "1ee5f32bd706f87d9c51",
+    "url": "/phaser-world-nav/static/js/runtime-main.30a1507b.js"
+  }
+]);
